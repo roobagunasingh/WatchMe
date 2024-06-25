5389ed9ee053134c1c80e287c102f16733170af0
stardustrooba <roobagunasingh@gmail.com>
2024-06-25 22:04:18 +0530

    movie grid js has been updated

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles.css';
 
 export default function MoviesGrid(){
-    return (
-       <div>
-        
-       </div>
-    )
+
+    const [movies, setMovies] = useState([]);
+
+        useEffect(() => {
+            fetch("movies.json")
+            .then(response => response.json())
+            .then(data => setMovies(data))
+        }, []);
+        return (
+       <div className="movies-grid">
+        {
+            movies.map(movie => (
+              <div key = { movie.id} className = 'movie-card'>
+                <img src={`images/${movie.image}`} alt = { movie.title}/>
+                <div classname='movie-card-info'>
+                    <h3 className ='movie-card-title'>{movie.title}</h3>
+                    <p className='movie-card-genre'>{movie.genre}</p>
+                    <p className='movie-card-rating'>{movie.rating}</p>
+                </div>
+              </div>  
+            ))
+        }
+        </div>
+        );
 }
