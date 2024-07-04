0382258bd740eac98b1821b659f2283d19009fae
stardustrooba <roobagunasingh@gmail.com>
2024-07-04 21:43:38 +0530

    tried out props

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import MovieCard from './MovieCard';
 import '../styles.css';
 
 export default function MoviesGrid(){
@@ -10,20 +11,14 @@ export default function MoviesGrid(){
             .then(response => response.json())
             .then(data => setMovies(data))
         }, []);
+
         return (
-       <div className="movies-grid">
-        {
-            movies.map(movie => (
-              <div key = { movie.id} className = 'movie-card'>
-                <img src={`images/${movie.image}`} alt = { movie.title}/>
-                <div classname='movie-card-info'>
-                    <h3 className ='movie-card-title'>{movie.title}</h3>
-                    <p className='movie-card-genre'>{movie.genre}</p>
-                    <p className='movie-card-rating'>{movie.rating}</p>
+              <div className="movies-grid">
+                { 
+                movies.map((movie) => (
+                    <MovieCard movie= {movie} key={movie.id}></MovieCard>
+                ))
+                }
                 </div>
-              </div>  
-            ))
-        }
-        </div>
         );
 }
