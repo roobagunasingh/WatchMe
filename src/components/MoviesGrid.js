0112c59086eb845d854349f95765247dc737ed29
stardustrooba <roobagunasingh@gmail.com>
2024-07-09 20:18:00 +0530

    Filter logic was implemented

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -6,6 +6,8 @@ export default function MoviesGrid(){
 
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [genre, setGenre] = useState("All Genres");
+    const [rating, setRating] = useState("All");
 
         useEffect(() => {
             fetch("movies.json")
@@ -15,11 +17,45 @@ export default function MoviesGrid(){
 
         const handleSearchChange = (e) => {
             setSearchTerm(e.target.value); 
-        }
+        };
 
-        const filteredMovies = movies.filter((movie) =>  
-            movie.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-        )
+        const handleGenreChange = (e) => {
+            setGenre(e.target.value);
+        };
+
+        const handleRatingChange = (e) => {
+            setRating(e.target.value);
+        };
+
+        const matchesGenres = (movie, genre) => {
+            return genre === "All Genres" || movie.genre.toLocaleLowerCase() === genre.toLocaleLowerCase();
+        };
+        
+        const matchesRating = (movie, rating) => {
+            switch(rating){
+                case 'All':
+                    return true;
+                case 'Good':
+                    return movie.rating >= 8;
+                case 'Ok':
+                    return movie.rating >= 5 && movie.rating < 8;
+                case 'Bad':
+                    return movie.rating < 5;
+                default:
+                    return false;
+            }
+        };
+
+        const matchesSearchTerm = (movie, searchTerm) => {
+            return movie.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+        };
+
+        const filteredMovies = movies.filter(
+            (movie) =>  
+                matchesGenres(movie, genre) &&
+                matchesRating(movie, rating) &&
+                matchesSearchTerm(movie, searchTerm)
+        );
 
         return (
               <div>
@@ -29,13 +65,35 @@ export default function MoviesGrid(){
                         placeholder="Search Movies..." 
                         value={searchTerm}
                         onChange = {handleSearchChange}/>
-                    <div className="movies-grid">
-                    { 
-                    filteredMovies.map((movie) => (
-                        <MovieCard movie= {movie} key={movie.id}></MovieCard>
-                    ))
-                    }
+                <div className="filter-bar">
+                    <div className="filter-slot">
+                        <label>Genre</label>
+                        <select className="filter-dropdown" value={genre} onChange={handleGenreChange}>
+                            <option>All Genres</option>
+                            <option>Comedy</option>
+                            <option>Romance</option>
+                            <option>Fantasy</option>
+                            <option>Drama</option>
+                        </select>
+                    </div>
+                    <div className="filter-slot">
+                        <label>Rating</label>
+                        <select className="filter-dropdown" value={rating} onChange={handleRatingChange}>
+                            <option>All</option>
+                            <option>Good</option>
+                            <option>Ok</option>
+                            <option>Bad</option>
+                        </select>
                     </div>
                 </div>
+
+                <div className="movies-grid">
+                { 
+                filteredMovies.map((movie) => (
+                    <MovieCard movie= {movie} key={movie.id}></MovieCard>
+                ))
+                }
+                </div>
+            </div>
         );
 }
