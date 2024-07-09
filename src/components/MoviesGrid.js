c7b1e157e476443e8dc9bf55372f870658ba5cd0
stardustrooba <roobagunasingh@gmail.com>
2024-07-09 19:36:58 +0530

    Searchterm scenario has been added

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,6 +5,7 @@ import '../styles.css';
 export default function MoviesGrid(){
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
         useEffect(() => {
             fetch("movies.json")
@@ -12,13 +13,29 @@ export default function MoviesGrid(){
             .then(data => setMovies(data))
         }, []);
 
+        const handleSearchChange = (e) => {
+            setSearchTerm(e.target.value); 
+        }
+
+        const filteredMovies = movies.filter((movie) =>  
+            movie.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        )
+
         return (
-              <div className="movies-grid">
-                { 
-                movies.map((movie) => (
-                    <MovieCard movie= {movie} key={movie.id}></MovieCard>
-                ))
-                }
+              <div>
+                <input 
+                        type="text" 
+                        className="search-input" 
+                        placeholder="Search Movies..." 
+                        value={searchTerm}
+                        onChange = {handleSearchChange}/>
+                    <div className="movies-grid">
+                    { 
+                    filteredMovies.map((movie) => (
+                        <MovieCard movie= {movie} key={movie.id}></MovieCard>
+                    ))
+                    }
+                    </div>
                 </div>
         );
 }
